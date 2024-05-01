29648d6c9abf6146c5fd927c3a7f31873348541d
HelenaMarecka <helena.marecka05@gmail.com>
2024-05-01 19:33:16 +0100

    adding the button

--- Assets/Scripts_Codes/Canon_Behaviour.cs
+++ Assets/Scripts_Codes/Canon_Behaviour.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Canon_behaviour : MonoBehaviour
 {
     public AudioClip cannonShot;
+
+    public GameObject ui;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideUI();   
     }
 
     // Update is called once per frame
@@ -19,9 +21,26 @@ public class Canon_behaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        ShowUI();
         if(other.tag == "Player")
         {
             gameObject.GetComponent<AudioSource>().PlayOneShot(cannonShot);
         }
+    }
+
+    public void ShootCannon() 
+    {
+
+     gameObject.GetComponent<AudioSource>().PlayOneShot(cannonShot);   
+    }
+
+
+    public void HideUI() 
+    {
+        ui.SetActive(false);
+    }
+    public void ShowUI() 
+    {
+        ui.SetActive(true);
     }
 }
